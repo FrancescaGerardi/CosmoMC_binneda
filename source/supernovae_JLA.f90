0d76ff8b7af174b4b9bbed9807d2ab4800bcb723
Matteo Martinelli <martinelli@novamaris.lorentz.leidenuniv.nl>
2019-04-15 14:00:46 +0200

    fixed metallicity

--- source/supernovae_JLA.f90
+++ source/supernovae_JLA.f90
@@ -1247,8 +1247,8 @@
         intpsi        = my_ispline(zhel, zint, tab_rhostar, b1, c1, d1, samp_size)
         metal_rhostar = (1-CMB%metal_R)*intpsi
         metal_Zb      = CMB%metal_y*(metal_rhostar/((omegab)*rhoc))*(3.0857e13/(3600*24*365.25))
-        metalsys      = -2.5*log10((1-0.18*metal_Zb)/(Zsun*(1-metal_Zb/Zsun)))-0.191   !metallicity systematic
-        
+        metalsys      = -2.5*log10(1-0.18*(metal_Zb/Zsun)*(1-0.10*metal_Zb/Zsun))-0.191   !metallicity systematic
+
         lumdists(i) = lumdists(i) + phenosys + SFRsys + CMB%metal_switch*metalsys
     ENDDO
 
